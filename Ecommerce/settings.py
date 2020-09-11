c87966d657a527402f851675b13a3ad024fc0b4f
Ayoyimika Ajibade <ajibadeayoyimika@gmail.com>
2020-09-11 15:09:42 +0100

    deployments

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'wub2y13a7(-h@v*)1u^dym@kd9hj@&
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ayoyimika-ecommerce.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
